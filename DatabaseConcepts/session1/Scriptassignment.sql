85bf270b50adf021414a5dadf45212ad651a1999
priyank <priyank.kotak@aimdek.com>
2020-01-16 18:46:02 +0530

    commit after database concepts
    Signed-off-by: priyank <priyank.kotak@aimdek.com>

---
+++ DatabaseConcepts/session1/Scriptassignment.sql
@@ -0,0 +1,26 @@
+--create DATABASE 
+--CREATE DATABASE PriyankKotakDB;
+
+
+--create table
+--CREATE TABLE Individual (IndividualId int,FirstName Varchar(255),LastName Varchar(255),UserName Char(10));
+
+--//insert records
+--INSERT INTO Individual VALUES ( 6, 'Benny', 'Hill', 'hillbenny' );
+
+--//update records
+--UPDATE Individual SET UserName = 'funnyman' WHERE IndividualId = 6;
+
+--//delete records
+--DELETE FROM Individual WHERE IndividualId = 6;
+
+--//select records
+--SELECT * FROM Individual;
+
+--//count records
+
+--INSERT INTO Individual VALUES ( 6, 'Benny', 'Hill', 'hillbenny' );
+--INSERT INTO Individual VALUES ( 8, 'dhyan', 'Hillcipher', 'jenny' );
+--
+----//DISTINCT
+--SELECT DISTINCT(FirstName) FROM Individual;
